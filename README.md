533aace81ba136f5ebac1139042e65707748a587
Doru Budai <doru.budai@gmail.com>
2016-07-01 18:50:47 +0300

    Add README

---
+++ README.md
@@ -0,0 +1,2 @@
+# jass
+Just Another Space Shooter
--- src/jass/game_objects/play/healthbar.cc
+++ src/jass/game_objects/play/healthbar.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2015, Doru Catalin Budai. All rights reserved.
+// Copyright (c) 2015, Doru Budai. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 
 #include "jass/game_objects/play/healthbar.h"
